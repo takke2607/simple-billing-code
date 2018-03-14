2cd14fa556fcba1130964ca7dcc2725de1dbc52f
takke2607 <37206921+takke2607@users.noreply.github.com>
2018-03-14 19:20:32 +0530

    Add files via upload

---
+++ bill.py
@@ -0,0 +1,44 @@
+
+def bill(grand_total,grand_sqft,rate):
+    sr_no = input("enter serial number")
+    desc = raw_input("enter description")
+    height = input("enter height")
+    width = input("enter width")
+    pcs = input("enter pcs")
+    sq_ft = height * width * pcs
+    print "square feet:",sq_ft
+    current_amount = sq_ft * rate
+    print "current amount:",current_amount
+    grand_total = grand_total + current_amount
+    grand_sqft = grand_sqft + sq_ft
+    answer =raw_input("do you want to enter the second record? y/n")
+    if answer.lower() == 'y':
+             bill(grand_total,grand_sqft,rate)
+    elif answer == 'n':
+             print "grand square feet:",grand_sqft
+             print "grand total:",grand_total
+    else:
+             print "invalid input"
+    
+    
+print "shilp creation"
+grand_total = 0
+grand_sqft = 0
+rate = 0.79
+choice = raw_input("enter into biling system? y/n")
+if choice == 'y':
+     password = input("enter password")
+     if password == 2477:
+              bill(grand_total,grand_sqft,rate)
+     else:
+              print "error ...wrong password!"
+else:
+    exit
+
+
+
+
+    
+ 
+
+    
